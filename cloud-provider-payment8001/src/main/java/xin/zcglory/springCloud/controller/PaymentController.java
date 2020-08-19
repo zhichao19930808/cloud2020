ae8d10b7107e9552732b9d26451a46f5c0d1f57d
zhichao19930808 <zhichaoglory@163.com>
2020-08-20 01:50:46 +0800

    commons

--- cloud-provider-payment8001/src/main/java/xin/zcglory/springCloud/controller/PaymentController.java
+++ cloud-provider-payment8001/src/main/java/xin/zcglory/springCloud/controller/PaymentController.java
@@ -2,11 +2,7 @@ package xin.zcglory.springCloud.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-import xin.zcglory.springCloud.dao.PaymentMapper;
+import org.springframework.web.bind.annotation.*;
 import xin.zcglory.springCloud.entity.CommonResult;
 import xin.zcglory.springCloud.entity.Payment;
 import xin.zcglory.springCloud.service.PaymentService;
