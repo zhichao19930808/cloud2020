52d05620e64a2645d15df743aae919c0dad09171
zhichao19930808 <zhichaoglory@163.com>
2020-08-21 01:40:40 +0800

    eureka

--- cloud-consumer-order80/src/main/java/xin/zcglory/springCloud/ConsumerMain80.java
+++ cloud-consumer-order80/src/main/java/xin/zcglory/springCloud/ConsumerMain80.java
@@ -5,10 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class consumerMain80 {
+public class ConsumerMain80 {
 
 
     public static void main(String[] args) {
-        SpringApplication.run(consumerMain80.class, args);
+        SpringApplication.run(ConsumerMain80.class, args);
     }
 }
--- cloud-eureka-server7001/src/main/java/xin/zcglory/springCloud/EurekaMain7001.java
+++ cloud-eureka-server7001/src/main/java/xin/zcglory/springCloud/EurekaMain7001.java
@@ -1,14 +1,14 @@
 package xin.zcglory.springCloud;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
 @SpringBootApplication
-public class PaymentMain8001 {
-
+@EnableEurekaServer
+public class EurekaMain7001 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7001.class, args);
     }
 }
