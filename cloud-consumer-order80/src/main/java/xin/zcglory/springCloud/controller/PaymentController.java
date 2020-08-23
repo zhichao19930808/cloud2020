bb52f7affa10bd467260cdd460176c12e5e9b654
zhichao19930808 <zhichaoglory@163.com>
2020-08-23 16:14:13 +0800

    eureka集群3

--- cloud-consumer-order80/src/main/java/xin/zcglory/springCloud/controller/PaymentController.java
+++ cloud-consumer-order80/src/main/java/xin/zcglory/springCloud/controller/PaymentController.java
@@ -4,7 +4,6 @@ package xin.zcglory.springCloud.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import xin.zcglory.springCloud.entity.CommonResult;
