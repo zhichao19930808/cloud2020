78b70868dfb55f925574ab061a8bbc9be3d90073
zhichao19930808 <zhichaoglory@163.com>
2020-08-29 09:45:26 +0800

    Ribbon - RandomRule

---
+++ cloud-consumer-order80/src/main/java/xin/zcglory/myrule/MySelfRule.java
@@ -0,0 +1,16 @@
+package xin.zcglory.myrule;
+
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RandomRule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MySelfRule {
+
+    @Bean
+    public IRule myRule() {
+        //随机规则
+        return new RandomRule();
+    }
+}
--- cloud-consumer-order80/src/main/java/xin/zcglory/springCloud/ConsumerMain80.java
+++ cloud-consumer-order80/src/main/java/xin/zcglory/springCloud/ConsumerMain80.java
@@ -3,8 +3,11 @@ package xin.zcglory.springCloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import xin.zcglory.myrule.MySelfRule;
 
 @SpringBootApplication
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE",configuration = MySelfRule.class)
 public class ConsumerMain80 {
 
 
