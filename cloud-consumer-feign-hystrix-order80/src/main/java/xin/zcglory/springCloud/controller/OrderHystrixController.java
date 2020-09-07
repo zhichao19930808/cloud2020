94baba75c441f80521201e28bdf9502d0b8de04f
zhichao19930808 <zhichaoglory@163.com>
2020-09-08 00:07:54 +0800

    openFeign hystrix 超时降级

--- cloud-consumer-feign-hystrix-order80/src/main/java/xin/zcglory/springCloud/controller/OrderHystrixController.java
+++ cloud-consumer-feign-hystrix-order80/src/main/java/xin/zcglory/springCloud/controller/OrderHystrixController.java
@@ -1,5 +1,7 @@
 package xin.zcglory.springCloud.controller;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,11 +22,17 @@ public class OrderHystrixController {
         return result;
     }
     @GetMapping(value = "/consumer/payment/hystrix/slow")
+    @HystrixCommand(fallbackMethod = "slowFallbackMethod",commandProperties = {
+            @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds",value = "2000")
+    })
     public String slow(){
         String result = paymentService.slow();
         log.info("*******result:"+result);
         return result;
     }
 
-
+    //兜底方法
+    public String slowFallbackMethod(){
+        return "我是消费者80，对付支付系统繁忙请10秒钟后再试或者自己运行出错请检查自己,(┬＿┬)";
+    }
 }
