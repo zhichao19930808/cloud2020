c1c926c189845c01eaca809fa0c5d9e6d881c102
zhichao19930808 <zhichaoglory@163.com>
2020-09-08 03:00:41 +0800

    openFeign openFign集成hystrix 超时后会多调一次服务（超过4秒才会服务端打两次日志）

--- cloud-provider-hystrix-payment8001/src/main/java/xin/zcglory/springCloud/srivice/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/xin/zcglory/springCloud/srivice/PaymentService.java
@@ -16,7 +16,7 @@ public class PaymentService {
 
     public String slow() {
         try {
-            TimeUnit.SECONDS.sleep(5);
+            TimeUnit.SECONDS.sleep(4);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
